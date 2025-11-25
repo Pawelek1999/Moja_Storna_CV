21d2814a6278e4a6bfed234336867b1ed81078e5
Jakub Pawełek <jakub.pawelek99@gmail.com>
2025-11-25 23:12:04 +0100

    Dodanie NavigationBar i rozwijanego menu

--- scripts.js
+++ scripts.js
@@ -1,2 +1,8 @@
-let zmienna = 10;
-console.log("Wartość zmiennej to: " + zmienna);
+const toggles = document.querySelectorAll('.nav_button');
+
+toggles.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const parentItem = btn.closest('.nav_item_with_submenu');
+    parentItem.classList.toggle('nav_item--open');
+  });
+});
